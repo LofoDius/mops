a936a9e5906907fcd1530b6130ff1edabac13ce6
LofoD <evkaverin@gmail.com>
2021-03-23 12:28:13 +0300

    Почистил App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           :extent="imgExtent"
       />
 
-      <vl-layer-image id="xkcd">
+      <vl-layer-image id="map">
         <vl-source-image-static :url="imgUrl" :size="imgSize" :extent="imgExtent" :projection="projection"/>
       </vl-layer-image>
 
@@ -28,52 +28,23 @@
       </vl-overlay>
 
     </vl-map>
-    <div class="pagination">
-      <button @click="paginationClick">Назад</button>
-      <button @click="paginationClick">2</button>
-      <button @click="paginationClick">3</button>
-      <button @click="paginationClick">4</button>
-    </div>
   </div>
 </template>
 
 <script>
 import * as olExt from 'vuelayers/lib/ol-ext';
 
-let size = {
-  b2f3: [4000, 1720],
+let size = [4000, 1720]
 
-};
+let extent = [0, 0, ...size];
 
-let extent = [0, 0, ...size.b2f3];
-
-// let imgUrls = {
-//   firstBuilding: [
-//     'https://sun9-75.userapi.com/impg/sBibAjwIT4VjMFGlnnuAcLrYkMDbTi-85evMmg/CJH_XWLIOX0.jpg?size=630x839&quality=96&proxy=1&sign=f64ed832c88c068be814a5a2111b5920&type=album',
-//     'https://sun9-70.userapi.com/impg/k9_qAEdd_6yAY8NQ8k0f4PSQ7Y0159KACoZnuA/IiYOnp2JIcg.jpg?size=784x852&quality=96&proxy=1&sign=61cf4209902bb85a00206bbec0f981ad&type=album',
-//     'https://sun9-41.userapi.com/impg/rZgE0sHu-q7JlY1OG6zQfn3TQXEyR7TyP4vcOA/UQv4JmttsVI.jpg?size=612x858&quality=96&proxy=1&sign=43c877b6e384d92e9dbc26afe1851018&type=album'
-//   ],
-//   secondBuilding: [
-//     'https://sun9-76.userapi.com/impg/w6f396O6CHb7N0b3F27e_rDcjM7CJYvhVJEBZQ/U9omaisVeQY.jpg?size=581x831&quality=96&proxy=1&sign=da7ff6a9f44daa5706dc71bc93277523&type=album',
-//     'https://sun9-3.userapi.com/impg/2IyPpkPyGrosh8lTO0dsklHVTYZyE8PJcQfMTA/NIe5CjiAno8.jpg?size=713x840&quality=96&proxy=1&sign=7244fc8e4ee8a39306e02ce7014d2c5d&type=album',
-//     'https://sun9-57.userapi.com/impg/fuYSiG_cic6O9XaCSfSSlGPKhN5p9aXBQ1OYzg/fkGr3bHJVpM.jpg?size=591x810&quality=96&proxy=1&sign=721fe22271d42ef9be8fb7bf2ec2ea2b&type=album'
-//   ],
-//   test: 'https://i.imgur.com/HTTIrye.jpg'
-// };
-
-
-
-let customProj = {
-  b2f3: olExt.createProj({
-    code: 'xkcd-image',
+let customProj = olExt.createProj({
+    code: 'map-image',
     units: 'pixels',
     extent,
-  }),
+  })
 
-
-};
-
-olExt.addProj(customProj.b2f3);
+olExt.addProj(customProj);
 
 export default {
   name: "App",
@@ -93,10 +64,10 @@ export default {
       // VueLayers Settings
       zoom: 4,
       maxZoom: 5,
-      center: [size.b2f3[0] / 2, size.b2f3[1] / 2],
+      center: [size[0] / 2, size[1] / 2],
       rotation: 0,
-      projection: customProj.b2f3.getCode(),
-      imgSize: size.b2f3,
+      projection: customProj.getCode(),
+      imgSize: size,
       imgExtent: extent,
       paginationCoord: [50, 50],
       overlayCoordinate: [2000, 1000]
@@ -104,12 +75,7 @@ export default {
   },
 
   methods: {
-    paginationClick(e) {
-      if (e.target.innerText === 'Назад') {
-        return false;
-      }
-      this.floor = e.target.innerText - 2;
-    }
+
   }
 }
 </script>
@@ -125,17 +91,5 @@ export default {
 html,
 body {
   height: 100%;
-}
-
-button {
-  background: lightgray;
-  border: none;
-  margin: 2px;
-}
-
-.pagination {
-  position: absolute;
-  bottom: 1vh;
-  right: 2vw;
 }
 </style>
