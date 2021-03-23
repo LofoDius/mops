34fb10e7e9d0425d80c413f250654c2cb2e03763
Евгения Олеговна Акинина <eakinina@sportmaster.ru>
2021-03-23 16:07:40 +0300

    просчитаны координаты для полигонов активных корпусов

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,8 @@
         <vl-source-image-static :url="imgUrl" :size="imgSize" :extent="imgExtent" :projection="projection"/>
       </vl-layer-image>
 
-      <vl-feature id="point">
-        <vl-geom-point :coordinates="[4992, 992]"/>
+      <vl-feature id="polygon" :properties="{prop: 'value', prop2: 'value'}">
+        <vl-geom-polygon :coordinates="polygonsCoords"></vl-geom-polygon>
       </vl-feature>
 
       <vl-overlay id="overlay" :position="overlayCoordinate">
@@ -48,13 +48,7 @@ olExt.addProj(customProj);
 
 export default {
   name: "App",
-
-  computed: {
-    imgUrl() {
-      return 'https://i.imgur.com/sOs5eTN.jpeg'
-    }
-  },
-
+  
   data() {
     return {
       building: 'firstBuilding',
@@ -73,10 +67,60 @@ export default {
       overlayCoordinate: [2000, 1000]
     }
   },
+  
+  computed: {
+    imgUrl() {
+      return 'https://i.imgur.com/sOs5eTN.jpeg'
+    },
+    
+    polygonsCoords() {
+      const structuredPolygons = [
+        {
+        name: '1 корпус', 
+          coords: [
+          [1993, 1029], [2167, 1029], [2167, 1153], [2096, 1153], [2096, 1253], [2167, 1253], [2167, 1377], 
+          [1993, 1377], [1993, 1459], [2052, 1459], [2052, 1633], [2134, 1633], [2134, 1573], [2307, 1573], 
+          [2307, 1491], [2249, 1491], [2249, 1318], [2226, 1318], [2226, 1252], [2306, 1252], [2306, 1153], 
+          [2226, 1153], [2226, 1087], [2250, 1087], [2250, 914], [2308, 914], [2308, 832], [2135, 832], 
+          [2135, 773], [2052, 773], [2052, 947], [1993 , 947]
+          ],
+        },
+        {
+          name: '2 корпус', 
+          coords: [
+            [2767, 1254], [2846, 1254], [2846, 1319], [2822, 1319], [2822, 1492], [2765, 1492],
+            [2765, 1575], [2938, 1575], [2938, 1634], [3020, 1634], [3020, 1461], [3079, 1461],
+            [3079, 1378], [2905, 1378], [2905, 1254], [2977, 1254], [2977, 1155], [2905, 1155],
+            [2905, 1030], [3078, 1030], [3078, 949], [3020, 949], [3020, 775], [2937, 775],
+            [2937, 833], [2764, 833], [2764, 916], [2822, 916], [2822, 1090], [2846, 1090],
+            [2846, 1155], [2767, 1155] 
+          ]
+        },
+        {
+          name: '9 корпус',
+          coords: [
+            [3275, 1632], [3358, 1632], [3358, 1532], [3626, 1532], [3626, 1418], [3395, 1418],
+            [3395, 1327], [3578, 1327], [3578, 1362], [3695, 1362], [3695, 1143], [3578, 1143],
+            [3578, 1178], [3395, 1178], [3395, 989], [3626, 989], [3626, 873], [3358, 873],
+            [3358, 773], [3276, 773], [3276, 947], [3163, 947], [3163, 980], [3079, 980],
+            [3079, 1030], [3275, 1030], [3275, 1377], [3163, 1377], [3163, 1459], [3275, 1459]
+          ]
+        },
+        {
+          name: 'Б корпус',
+          coords: [
+            [3290, 314], [3932, 314], [3932, 45], [3290, 45]
+          ]
+        }
+      ];
+      
+      const resultArray = [];
+      structuredPolygons.forEach((polygon) => resultArray.push(polygon.coords))
+      return resultArray;
+    }
+  },
 
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
 
